3caa80e14579dc56654cbd06f227026dc536febd
Rodrigo Alves <igo.sud@hotmail.com>
2020-04-02 20:03:00 -0300

    updated gitpod link

--- platforms/browser/www/src/js/main.js
+++ platforms/browser/www/src/js/main.js
@@ -8,3 +8,14 @@ function openModal() {
 function closeModal() {
   modal.style.display = 'none'
 }
+
+function registerServiceWorker() {
+  // registrando o service worker para navegadores com suporte
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js', { scope: '/' }).then(() => {
+      console.log('Service Worker registrado com sucesso.');
+    }).catch(error => {
+      console.log('Service Worker falhou:', error);
+    });
+  }
+}
