0eb58a9603770fbbab61716ab3b274a599f51f1f
Rodrigo Alves <igo.sud@hotmail.com>
2020-04-02 21:22:21 -0300

    deploy pwa

--- platforms/browser/www/src/js/main.js
+++ platforms/browser/www/src/js/main.js
@@ -9,13 +9,13 @@ function closeModal() {
   modal.style.display = 'none'
 }
 
-function registerServiceWorker() {
+(function registerServiceWorker() {
   // registrando o service worker para navegadores com suporte
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js', { scope: '/' }).then(() => {
+    navigator.serviceWorker.register('../../sw.js', { scope: '/' }).then(() => {
       console.log('Service Worker registrado com sucesso.');
     }).catch(error => {
       console.log('Service Worker falhou:', error);
     });
   }
-}
+})()
--- www/sw.js
+++ www/sw.js
@@ -1,16 +1,41 @@
-self.addEventListener('install', e => {
+var CACHE_NAME = 'cache-reload-v1';
+
+self.addEventListener('install', function (e) {
   e.waitUntil(
-    // depois que o Service Worker estiver instalado,,
-    // abra um novo cache
-    caches.open('my-pwa-cache').then(cache => {
-      // adicione todas as URLs de recursos que queremos armazenar em cache
+    caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll([
         '/',
         '/index.html',
-        '/manifest.json',
-        '/assets/*',
-        '/scr/*',
+        '/src/css/calendar.css',
+        '/src/css/main.css',
+        '/src/js/calendar.js',
+        '/src/js/main.js',
+        '/src/js/swipe.js',
+        '/assets/logo.png'
       ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function activator(event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys
+        .filter(function (key) {
+          return key.indexOf(CACHE_NAME) !== 0;
+        })
+        .map(function (key) {
+          return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (cachedResponse) {
+      return cachedResponse || fetch(event.request);
     })
   );
 });
